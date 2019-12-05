653ee2c268cb79d6064437b3b29a00bd8dfcaaf8
Eduard Spack <edi.spack@gmail.com>
2019-12-05 23:12:15 +0200

    Initial commit.

---
+++ Fractal/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "2X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnZoomIn);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(662, 489);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "0.5X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnZoomOut);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 489);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnReset);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(662, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.OnPrecisionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(586, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Precision:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mandelbrot",
+            "Mandelbrot 3",
+            "Mandelbrot 4"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.OnFractalChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fractal";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnClick);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Fractal/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int w = 800, h = 600, p, f_index;
+        double cx, cy, wx, r;
+        bool firstPaint = true;
+        Bitmap bm = new Bitmap(800, 600);
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            if(firstPaint)
+            {
+                this.comboBox1.SelectedIndex = 0;
+                f_index = 0;
+                cx = 0.0;
+                cy = 0.0;
+                wx = 8.0;
+                r = wx / (double)w;
+                Redraw();
+                firstPaint = false;
+            }
+        }
+
+        private void OnClick(object sender, MouseEventArgs e)
+        {
+            cx = cx + ((double)(e.X - w / 2) * r);
+            cy = cy - ((double)(e.Y - h / 2) * r);
+            Redraw();
+        }
+
+        private void OnZoomIn(object sender, EventArgs e)
+        {
+            wx = wx * 0.5;
+            r = wx / (double)w;
+            Redraw();
+
+            this.button2.Enabled = true;
+            if (wx <= 0.00000001)
+            {
+                this.button1.Enabled = false;
+            }
+        }
+
+        private void OnZoomOut(object sender, EventArgs e)
+        {
+            wx = wx * 2.0;
+            r = wx / (double)w;
+            Redraw();
+
+            this.button1.Enabled = true;
+            if(wx >= 8.0)
+            {
+                this.button2.Enabled = false;
+            }
+        }
+
+        private void OnFractalChanged(object sender, EventArgs e)
+        {
+            f_index = this.comboBox1.SelectedIndex;
+            cx = 0.0;
+            cy = 0.0;
+            wx = 8.0;
+            r = wx / (double)w;
+            this.button1.Enabled = true;
+            this.button2.Enabled = false;
+            Redraw();
+        }
+
+        private void OnPrecisionChanged(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        private void OnReset(object sender, EventArgs e)
+        {
+            cx = 0.0;
+            cy = 0.0;
+            wx = 8.0;
+            r = wx / (double)w;
+            this.button1.Enabled = true;
+            this.button2.Enabled = false;
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            int i, j;
+            p = (int)this.numericUpDown1.Value;
+            Graphics g = this.CreateGraphics();
+            for (i = 0; i < w; i++)
+            {
+                for (j = 0; j < h; j++)
+                {
+                    if (IsInSet((double)(i - w / 2) * r + cx, cy - (double)(j - h / 2) * r) == p)
+                    {
+                        bm.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        bm.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            g.DrawImage(bm, 0, 0, w, h);
+
+            //g.DrawLine(new Pen(Color.Red), 0, h / 2, w, h / 2);
+            //g.DrawLine(new Pen(Color.Red), w / 2, 0, w / 2, h);
+        }
+
+        private int IsInSet(double c_re, double c_im)
+        {
+            int i;
+            double z_re, z_im;
+            z_re = 0;
+            z_im = 0;
+            i = 0;
+            while(i < p)
+            {
+                if(f_index == 0)
+                {
+                    (z_re, z_im) = Mandelbrot(z_re, z_im, c_re, c_im);
+                }
+                else if(f_index == 1)
+                {
+                    (z_re, z_im) = Mandelbrot3(z_re, z_im, c_re, c_im);
+                }
+                else if (f_index == 2)
+                {
+                    (z_re, z_im) = Mandelbrot4(z_re, z_im, c_re, c_im);
+                }
+                
+                if(z_re*z_re + z_im*z_im > 4)
+                {
+                    break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        private (double re, double im) Mandelbrot(double zn_re, double zn_im, double c_re, double c_im)
+        {
+            return (zn_re*zn_re - zn_im*zn_im + c_re, 2*zn_re*zn_im + c_im);
+        }
+
+        private (double re, double im) Mandelbrot3(double zn_re, double zn_im, double c_re, double c_im)
+        {
+            return (zn_re * zn_re * zn_re - 3 * zn_re * zn_im * zn_im + c_re, 3 * zn_re * zn_re * zn_im - zn_im * zn_im * zn_im + c_im);
+        }
+
+        private (double re, double im) Mandelbrot4(double zn_re, double zn_im, double c_re, double c_im)
+        {
+            return (zn_re * zn_re * zn_re * zn_re - 6 * zn_re * zn_re * zn_im * zn_im + zn_im * zn_im * zn_im * zn_im + c_re, 4 * zn_re * zn_re * zn_re * zn_im - 4 * zn_re * zn_im * zn_im * zn_im + c_im);
+        }
+    }
+}
